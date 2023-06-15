9db6f4e5303a38a725a76229ebe35448a697df12
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-06-15 18:42:19 +0300

    Update youtube-parser.py

--- youtube-parser.py
+++ youtube-parser.py
@@ -1,1 +1,14 @@
+# Получить ID и название ролика с Yutube.
+# Передаем HTML-код в объект BeautifulSoup, затем извлекаем значения из тега iframe.
+# Используем метод split для получения конечной части значения src (jpVpmg7qci0).
 
+from bs4 import BeautifulSoup
+
+html = '<iframe width="560" height="315" src="https://www.youtube.com/embed/jpVpmg7qci0" title="YouTube video player" frameborder="0" allow="accelerometer"></iframe>'
+soup = BeautifulSoup(html, 'html.parser')
+
+src = soup.iframe['src'].split('/')[-1]
+title = soup.iframe['title']
+
+print(src)
+print(title)
